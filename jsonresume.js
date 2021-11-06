40cdc1ad8c0db57fd67f19101f7135608273450b
PoorniRam <91675540+PoorniRam@users.noreply.github.com>
2021-11-07 00:16:28 +0530

    Add files via upload
    json resume

---
+++ jsonresume.js
@@ -0,0 +1,34 @@
+RESUME={
+  "basics": {
+    "name": "Poornima",
+    "label": "Developer",
+      "email": "poorniram01@gmail.com",
+    
+    "location": {
+      
+      "city": "Madurai",
+      "countryCode": "India",
+      
+    }
+},
+
+
+"education": {
+   
+    "studyType": "Bachelor of engineering and technology",
+     "Course": "Textile technolgy" 
+  },
+
+"skills": {
+    "name": "Web Development",
+     "keywords": [
+      "HTML",
+      "CSS",
+      "JavaScript",
+
+    ]
+  },
+
+    
+}
+
